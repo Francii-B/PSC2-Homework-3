78fdf1f6a62d33d34250ba5f6cb3cf480df92412
Francii-B <33251358+Francii-B@users.noreply.github.com>
2017-12-11 21:03:06 +0100

    Update Random_list.py
    1) The parameters of the list are changed (to increase the value of the first element)
    2) Use the function 'repeat', instead of timeit, to repeat the measurement different times
    3) In each set of measurements, take the median, because it is less influenced by outliers
    4) No random elements are fixed; deletion and get of a random element sum two times: the time to find a random value, and the time to run the fuction

--- Random_list.py
+++ Random_list.py
@@ -1,14 +1,11 @@
-import random
-import timeit
-import numpy as np
-import Sort_Module as s
-import Max_Heap as h
-import Tree_Module as t
+import random, timeit, statistics as st, numpy as np
+import Sort_Module as s, Max_Heap as h, Tree_Module as t
 import matplotlib.pyplot as plt
+
 
 #a: generate the different lenght of the lists
 #b: generate the largest list. Every smaller list is taken from it
-a = np.logspace(0.5, np.log10(10000, dtype=float), base=10, endpoint=False, dtype=int)
+a = np.logspace(1.4, np.log10(10000, dtype=float), base=10, endpoint=False, dtype=int)
 b = random.choices(range(a[-1] + 1), k=a[-1])
 
 
@@ -17,35 +14,33 @@ def Sorting_t(a,b):
     c=[]
     d=[]
     for i in a:
-        ct=timeit.timeit(lambda: s.merge(b[:i]),number=10 )
+        ct=st.median(timeit.repeat(lambda: s.merge(b[:i]),repeat=10,number=1))
         c.append(ct)
-        dt=timeit.timeit(lambda : s.QuickSort(b[:i]), number=10)
+        dt=st.median(timeit.repeat(lambda : s.QuickSort(b[:i]), repeat=10,number=1))
         d.append(dt)
 
+    plt.figure(0)
     Show_graph(a,[c,d], ['Merge sort', 'Quick sort'])
     plt.title('Sorting time')
-    plt.show()
-
-#Measurements of the Binary Search Tree (insertion of the whole tree, get and delete and insert a random element, find max)
+    
+#Measurements of the Binary Search Tree (insertion of the whole tree, get and delete a random element, find max)
 def Tree_t(a,b,Graph=True):
-    Tree_d={'tot':[], 'init_l' : [] , 'max_l': [], 'get_l':[] , 'delete_l':[], 'insert_l':[]}
+
+    Tree_d={'tot':[], 'init_l' : [] , 'max_l': [], 'get_l':[] , 'delete_l':[]}
     for i in a:
-        random_el = random.choice(b[:i]) #use always the same random element
-        #measure the time
+        #measure the time, and find the median
         bst = t.BinarySearchTree()
-        init_t = timeit.timeit(lambda: bst.insertArr(b[:i]), number=10)/10
-        max_t= timeit.timeit(lambda: bst.findMax(), number=10)/10
-        get_t= timeit.timeit(lambda: bst.get(random_el), number=10)/10
-        insert_t = timeit.timeit(lambda: bst.put(random_el), number=10)/10
-        delete_t=timeit.timeit(lambda: bst.delete(random_el), number=10)/10
-        
+        init_t =st.median(timeit.repeat(lambda: bst.insertArr(b[:i]), repeat=10,number=1))
+        max_t= st.median(timeit.repeat(lambda: bst.findMax(),repeat=40,number=1))
+        get_t= st.median(timeit.repeat(lambda: bst.get(random.choice(bst.tree_el)),repeat=40,number=1))
+        delete_t= st.median(timeit.repeat(lambda: bst.delete(random.choice(bst.tree_el)),repeat=20,number=1))
+
         #record the datas
         Tree_d['init_l'].append(init_t)
         Tree_d['max_l'].append(max_t)
         Tree_d['get_l'].append(get_t)
         Tree_d['delete_l'].append(delete_t)
-        Tree_d['insert_l'].append(insert_t)
-        Tree_d['tot'].append(init_t + max_t + get_t + delete_t+ insert_t)
+        Tree_d['tot'].append(init_t + max_t + get_t + delete_t)
 
     if Graph: #plot only if it is required
         plt.figure(1)
@@ -55,31 +50,29 @@ def Tree_t(a,b,Graph=True):
         plt.subplot(212)
         Show_graph(a,[Tree_d['init_l']], ['Insertion Time'])
 
+
         plt.figure(2)
         plt.title('Binary Search Tree pt.2')
-        Show_graph(a, [Tree_d['get_l'],Tree_d['delete_l'],Tree_d['max_l'], Tree_d['insert_l']], ['Random Get', 'Random Delete', 'Find max', 'Random Insert'])
-        plt.show()
+        Show_graph(a, [Tree_d['get_l'],Tree_d['delete_l'],Tree_d['max_l']], ['Random Get', 'Random Delete', 'Find max'])
 
     return Tree_d
 
-#Measurements for Max Heap (insertion of the whole array, insetion of a random element, get max, delete max)
+
+#Measurements for Max Heap (insertion of the whole array, get max, delete max)
 def Heap_t(a,b, Graph=True):
-    Heap_d={'tot':[],'init_l':[], 'max_l':[], 'deleteM_l':[], 'insert_l': []}
+    Heap_d={'tot':[],'init_l':[], 'max_l':[], 'deleteM_l':[]}
 
     for i in a:
-        random_el = random.choice(b[:i]) #generate a random element
         #measure the time
         mhp = h.MaxBinHeap()
-        init_t = (timeit.repeat(lambda: mhp.insertArr(b[:i]), number=1, repeat= 10))[0]
-        max_t = min(timeit.repeat(lambda: mhp.getMax(), number=1, repeat= 20))
-        deleteM_t = min(timeit.repeat(lambda: mhp.delMax(), number=1, repeat= 10))
-        insert_t = min(timeit.repeat(lambda: mhp.insert(random_el), number=1, repeat= 5))
+        init_t = st.median(timeit.repeat(lambda: mhp.insertArr(b[:i]),repeat=10, number=1))
+        max_t = st.median(timeit.repeat(lambda: mhp.getMax(),repeat=10, number=1))
+        deleteM_t = st.median(timeit.repeat(lambda: mhp.delMax(),repeat=30, number=1))
         #record the datas
         Heap_d['init_l'].append(init_t)
         Heap_d['max_l'].append(max_t)
         Heap_d['deleteM_l'].append(deleteM_t)
-        Heap_d['insert_l'].append(insert_t)
-        Heap_d['tot'].append(init_t + max_t + deleteM_t + insert_t)
+        Heap_d['tot'].append(init_t + max_t + deleteM_t)
 
     if Graph: #plot only if it is required
         plt.figure(3)
@@ -87,17 +80,18 @@ def Heap_t(a,b, Graph=True):
         plt.title('Max Heap pt.1')
         Show_graph(a,[Heap_d['tot']],['Total time'])
         plt.subplot(212)
-        Show_graph(a,[Heap_d['init_l']],['Total Insertion'])
+        Show_graph(a,[Heap_d['init_l']],['Element Insertion'])
+
 
         plt.figure(4)
         plt.title('Max Heap pt.2')
-        Show_graph(a, [Heap_d['max_l'],Heap_d['deleteM_l'], Heap_d['insert_l']],['Find max', 'Random delete', 'Random Insert'])
-        plt.show()
+        Show_graph(a, [Heap_d['max_l'],Heap_d['deleteM_l']],['Find max', 'Max delete'])
     return Heap_d
 
 #Comparisons between the Tree and the Heap (total time, insertion of the whole list, insertion of a random element, get max)
 def Comparisons(TD, HD):
     plt.figure(5)
+
     plt.subplot(211)
     plt.title('Comparisons between Heap and Tree')
     Show_graph(a, [TD['tot'], HD['tot']],['Tree total time','Heap total time'])
@@ -105,22 +99,18 @@ def Comparisons(TD, HD):
     plt.subplot(212)
     Show_graph(a, [TD['init_l'], HD['init_l']], ['Tree insertion', 'Heap insertion'])
 
-    plt.figure()
-    plt.title('Comparisons between Heap and Tree')
-    Show_graph(a, [TD['insert_l'], HD['insert_l'], TD['max_l'], HD['max_l']], 
-               ['Tree Random Insertion', 'Heap Random Insertion', 'Tree Get Max', 'Heap Get Max'])
-    plt.show()
 
 #Plot the data when it is required
 def Show_graph(a,l,lab):
     plt.grid(True)
     plt.xscale('symlog')
-    plt.xlim(10,8800)
+    plt.xlim(a[0],a[-1]+1)
     for j in range(len(lab)):
         plt.plot(a, l[j], '.-', label=lab[j])
     plt.ylabel('Time')
     plt.xlabel('Lenght of the list')
     plt.legend(loc='upper left', shadow=True)
+
 
 #Ask what the user wants to visualize
 def Questions():
@@ -143,5 +133,7 @@ def Questions():
             Tree_t(a,b)
         if answ_l[2]:
             Heap_t(a,b)
+    if True in answ_l:
+        plt.show()
 
 Questions()
